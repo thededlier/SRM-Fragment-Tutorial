3dcef61d567c7549fc16f601eb4fe47f23eb44ae
Rohan Anand <thededlier@gmail.com>
2016-10-07 11:37:25 +0530

    IPSum with articles

---
+++ app/src/main/java/com/example/rohan/fragmentstutorial/IPSum.java
@@ -0,0 +1,15 @@
+package com.example.rohan.fragmentstutorial;
+
+public class IPSum {
+
+    static String[] Headlines = {
+            "About SRM",
+            "SRM Vision"
+    };
+
+    static String[] Articles = {
+            "Article One\n\nSRM University is one of the top ranking universities in India with 33,044 students and 2358 faculty across all the campus, offering a wide range of undergraduate, postgraduate and doctoral programs in Engineering, Management, Medicine and Health sciences, and Science and Humanities.",
+            "Article Two\n\nTo emerge as a World - Class University in creating and disseminating knowledge, and providing students a unique learning experience in science, technology, medicine, management and other areas of scholarship that will best serve the world and betterment of mankind."
+    };
+
+}
